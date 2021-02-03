85be4d33f5b1fa03e35c79f5dc5d4735298fbffb
Satya Sharma <satyasharma2964@gmail.com>
2021-02-03 16:37:49 +0530

    Adding Background music

---
+++ js/music.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", event => {
+
+  // This is Code is used when audio src is passed in html boilerplate
+  const audio = document.querySelector("audio");
+  audio.volume = 0.2;
+  audio.play();
+})
